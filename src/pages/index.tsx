761eb4c79fe8c0db01703fb436f3c6c47634bdaa
Evellyn Lima <evellynlrl@hotmail.com>
2021-07-17 20:20:54 -0300

    Add introduction block

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,24 @@ import s from 'styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <main >
+    <main>
       <Head>
         <title>Bruna Freitas</title>
         <meta name="description" content="Bruna Freitas | Product Designer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <section className={s.apre}>
-        conteúdo
+      <section className={s.introduction}>
+        <div className="container">
+          <h1>hello, I&apos;m <span>Bruna.</span></h1>
+          <p>Product designer with +4 years of experience. Working on end to end design process, from discovery to delivery. Strategy focused and always in search for the right problems to solve.</p>
+          <ul>
+            <li>💡 empowering commerce at <strong>olist</strong></li>
+            <li>📌 living in São Paulo, Brazil</li>
+            <li>🎧 listening to this <strong>playlist</strong></li>
+          </ul>
+        </div>
       </section>
-
     </main>
   )
 }
