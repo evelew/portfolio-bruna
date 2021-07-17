3849d7a03b6bc6cecc84f840da369f54714ebb37
Evellyn Lima <evellynlrl@hotmail.com>
2021-07-17 19:50:23 -0300

    Add support to sass. Add src folder. Update globals.scss file. Add baseUrl config.

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+import s from 'styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <main >
+      <Head>
+        <title>Bruna Freitas</title>
+        <meta name="description" content="Bruna Freitas | Product Designer" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <section className={s.apre}>
+        conteúdo
+      </section>
+
+    </main>
+  )
+}
