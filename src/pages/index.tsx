cc30b0958b4108f993465a3b5be3b77ce4b938d8
Evellyn Lima <evellynlrl@hotmail.com>
2021-07-21 21:26:33 -0300

    Add nerd face emoji

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
+import imageNerdFaceEmoji from 'public/images/nerd-face.png'
 import image10Segundos from 'public/images/10-segundos.png'
 import image10SegundosScoreSystem from 'public/images/10-segundos-score-system.png'
 import imageIntegraAde02 from 'public/images/integra-ade02.png'
@@ -20,19 +22,18 @@ export default function Home() {
 
       <section className={s.introduction}>
         <div className="container">
+          <div className={s['introduction__emoji']}>
+            <Image src={imageNerdFaceEmoji} alt="Nerd face emoji" />
+          </div>
+
           <h1 className={s['introduction__title']}>
             hello, I&apos;m{' '}
             <span className={s['introduction__title-name']}>
               Bruna.
               <span className={s['introduction__title-svg']}>
-                <svg
-                  width="273"
-                  height="131"
-                  viewBox="0 0 273 131"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
+                <svg width="273" height="131" viewBox="0 0 273 131" fill="none">
                   <path
+                    className={s.path}
                     d="M206.571 15.2966C189.158 18.3676 171.746 21.4387 154.333 24.5098C123.975 29.8642 93.4353 34.8777 63.2574 41.1435C48.7703 44.1514 31.1386 47.0522 20.0689 55.9868C11.4716 62.9258 4.61591 74.6853 6.75102 83.5318C11.9704 105.157 53.2492 106.88 78.067 107.369C106.509 107.929 130.802 103.245 163.473 95.2059C188.369 89.08 209.477 82.461 225.454 74.815C239.219 68.2274 248.322 61.7419 255.144 55.8221C263.31 48.7367 269.321 38.9324 259.189 32.7811C251.959 28.3911 241.707 25.629 232.67 23.6005C215.507 19.7484 197.242 18.1334 178.642 18.511C142.373 19.2473 102.748 25.2703 68.6784 37.9055C56.1329 42.5581 37.1074 50.8672 34.2571 62.0653C32.0992 70.5432 35.1225 80.6415 39.1703 88.035C44.3474 97.4913 53.8499 105.033 65.9282 109.89C80.1472 115.609 96.0791 114.537 112.817 112.816C129.087 111.144 141.603 108.026 159.244 102.56C177.704 96.8403 186.967 93.9964 204.419 86.3858C214.17 82.1336 229.3 72.8319 229.3 72.8319C229.3 72.8319 249.568 50.2668 250.239 48.8265"
                     stroke="#FFA3A4"
                     strokeWidth="4"
