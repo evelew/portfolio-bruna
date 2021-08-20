12cd61d1f8a39e941d677e98ea2a1e94dfc08a4b
Evellyn Lima <evellynlrl@hotmail.com>
2021-08-20 20:54:46 -0300

    Add logic to show/hide fixed button

--- src/components/go-to-top-button/index.tsx
+++ src/components/go-to-top-button/index.tsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react'
+
 import ArrowButtonSvg from 'public/icons/arrow-button.svg'
 
 import s from './styles.module.scss'
 
 const GoTotopButton = () => {
+  const [opacity, setOpacity] = useState(0)
+
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      const scrollPosition = window.pageYOffset
+
+      if (scrollPosition <= window.innerHeight / 2) {
+        setOpacity(0)
+      } else {
+        setOpacity(1)
+      }
+    })
+  }, [])
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -11,7 +27,12 @@ const GoTotopButton = () => {
   }
 
   return (
-    <div className={`container ${s.wrapper}`}>
+    <div
+      className={`container ${s.wrapper}`}
+      style={{
+        opacity: opacity
+      }}
+    >
       <button className={s.button} onClick={scrollToTop}>
         <ArrowButtonSvg />
       </button>
