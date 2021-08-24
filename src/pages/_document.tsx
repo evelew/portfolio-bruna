a3bcc2098b74e9118a945aed5977b2d06521daec
Evellyn Lima <evellynlrl@hotmail.com>
2021-08-23 21:19:46 -0300

    Add analytics script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+const analyticsID = process.env.ANALYTICS_ID
+
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Red+Hat+Display:wght@400;500;700&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Red+Hat+Display:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${analyticsID}`}
+          />
         </Head>
-        
+
         <body>
           <Main />
           <NextScript />
